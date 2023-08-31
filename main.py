a1786d748ee51f5b9594fca366453f1a61d6d06d
Abha Sharma <abha25sharma@gmail.com>
2023-08-31 13:00:20 -0700

    Added loop to keep the game going

--- main.py
+++ main.py
@@ -2,58 +2,75 @@ from turtle import Turtle
 from turtle import Screen
 import random
 
-COLORS=['red','orange','yellow','green','blue','indigo','violet']
+COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 FINISH_LINE = 235
-turtles= []
+turtles = []
 screen = Screen()
 screen.setup(width=500, height=400)
-
-
-for _ in range(0,7):
-    tim=Turtle()
-    tim.penup()
-    tim.shape('turtle')
-    tim.color(COLORS[_])
-    tim.setx(-200)
-    tim.sety(150-(_*50))
-    turtles.append(tim)
-
 screen.listen()
-user_turtle = screen.textinput(title="Bet on a turtle",prompt="Type a color from VIBGYOR")
-end_pos_x = False
-winner=[]
-winner_count = 0
-while not end_pos_x:
-    for turtle_index in turtles:
-        turtle_index.setx(turtle_index.xcor()+random.randint(0,10))
-        if turtle_index.xcor() >= FINISH_LINE:
-            turtles.remove(turtle_index)
-            winner_count += 1
-            if winner_count > 3:
-                end_pos_x=True
-                break
-            winner.append(turtle_index.pencolor())
-
-# if user_turtle.lower() != winner.lower():
-# if winner[0]== user_turtle:
-#     print(f"Winner is {winner[0]} turtles. Collect your prize.")
-# else:
-#     print(f"Your {user_turtle} turtle lost the race. Winner is {winner[0]} turtle.")
 
 
-print(f"Winners are: ")
-winner_list = enumerate(winner, start= 1)
-for (index,win) in winner_list:
-    print(f"Position# {index}: {win}")
-# print(list(winner_list))
+def setup():
+    global turtles
+    turtles = []
+    for _ in range(0, 7):
+        tim = Turtle()
+        tim.penup()
+        tim.shape('turtle')
+        tim.color(COLORS[_])
+        tim.setx(-200)
+        tim.sety(150 - (_ * 50))
+        turtles.append(tim)
+
+    for index in turtles:
+        print(index.color())
 
 
-if user_turtle in winner:
-    print("Your turtle won a position. Please collect your prize.")
-else:
-    print("Your turtle did not win this time.")
+def play():
+    user_turtle = screen.textinput(title="Bet on a turtle", prompt="Type a color from VIBGYOR")
+    end_pos_x = False
+    winner = []
+    winner_count = 0
+    while not end_pos_x:
+        for turtle_index in turtles:
+            turtle_index.setx(turtle_index.xcor() + random.randint(0, 10))
+            if turtle_index.xcor() >= FINISH_LINE:
+                turtles.remove(turtle_index)
+                winner_count += 1
+                if winner_count > 3:
+                    end_pos_x = True
+                    break
+                winner.append(turtle_index.pencolor())
+
+    # if user_turtle.lower() != winner.lower():
+    # if winner[0]== user_turtle:
+    #     print(f"Winner is {winner[0]} turtles. Collect your prize.")
+    # else:
+    #     print(f"Your {user_turtle} turtle lost the race. Winner is {winner[0]} turtle.")
+
+    print(f"Winners are: ")
+    winner_list = enumerate(winner, start=1)
+    for (index, win) in winner_list:
+        print(f"Position# {index}: {win}")
+    # print(list(winner_list))
+
+    if user_turtle in winner:
+        print("Your turtle won a position. Please collect your prize.")
+    else:
+        print("Your turtle did not win this time.")
 
 
+continue_play = True
+while continue_play:
+    setup()
+    play()
+    user_input = screen.textinput(prompt="Enter yes or no.", title="Do you want to play again?")
+    if user_input.lower() == 'yes':
+        continue_play = True
+        screen.clear()
+    else:
+        continue_play=False
+exit()
 
 #
 # def move_right():
